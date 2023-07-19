59ff565e35107640fe6b213d03c3fa7d24187bda
kemboi eliud <kemboieliud2019@gmail.com>
2023-07-19 03:14:44 -0700

    README.md

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,12 @@
+int _putchar(char c);
+void print_alphabet(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_jack_baver(void);
+void times_tablet(void);
+int add(int,int);
+void print_to_98(int n);
+void print_times_tablet(int n);
