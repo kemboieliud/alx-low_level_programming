34032ad82d20f0af7015732737c187858c0fed2a
kemboi eliud <kemboieliud2019@gmail.com>
2023-07-21 01:11:55 -0700

    low

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,8 @@
 #include "main.h"
+
 /**
- * main -Entry point of my code,
- *
- * Return: always 0
+ * main -  prints _putchar, followed by a new line
+ * Return: Always 0 (Success)
  */
 
 int main(void)
@@ -16,5 +16,6 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
+
 	return (0);
 }
